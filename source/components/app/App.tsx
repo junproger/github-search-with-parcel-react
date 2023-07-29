0251e36158612fbcf83b1c69cc4236d216b2706f
Junior Programmer <progphyton@gmail.com>
2023-07-29 19:06:52 +0300

    refactor: move constants to separate file

--- source/components/app/App.tsx
+++ source/components/app/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import github from '../../assets/github-logo.png';
 
 import { Header } from '../view/Header';
 import { Search } from '../control/Search';
+import Texts from '../../constants/textCollection';
 
 export const App: React.FC = () => {
-  const titleHeader = 'Github-Search React-App';
-  const informSearch =
-    'For enter print a name or part of name and press an enter key. For delete printed press a delete key.';
-  const searchHolder = 'Enter a name or part of name, "octocat" for example.';
-  const titleSearch = 'Enter a name or part of name and press an enter key, for delete press a delete key.';
+  const [getQuery, setQuery] = useState('');
   const callback = (value: string): void => {
+    setQuery(value);
     // eslint-disable-next-line no-console
-    console.log(value);
+    console.log(value, getQuery);
   };
   return (
     <>
-      <Header image={github} attrib="github" title={titleHeader} />
-      <Search callback={callback} holder={searchHolder} inform={informSearch} title={titleSearch} />
+      <Header image={github} attrib="github" title={Texts.titleHeader} />
+      <Search callback={callback} holder={Texts.searchHolder} inform={Texts.informSearch} title={Texts.titleSearch} />
     </>
   );
 };
---
+++ source/constants/textCollection.ts
@@ -0,0 +1,8 @@
+enum textCollection {
+  titleHeader = 'Github-Search React-App',
+  informSearch = 'For enter print a name or part of name and press an enter key. For delete printed press a delete key.',
+  searchHolder = 'Enter a name or part of name, "octocat" for example.',
+  titleSearch = 'Enter a name or part of name and press an enter key.',
+}
+
+export default textCollection;
