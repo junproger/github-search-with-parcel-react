88f2e3db912c646bb615ae3ee235411ca409cc68
Junior Programmer <progphyton@gmail.com>
2023-07-30 23:21:51 +0300

    feat: add sort-order queries

---
+++ source/constants/apiSortOrder.ts
@@ -0,0 +1,7 @@
+export const SortOrder = {
+  BestMatch: '&sort=bestmatches&order=desc',
+  HighRepo: '&sort=repositories&order=desc',
+  LowRepo: '&sort=repositories&order=asc',
+  HighFoll: '&sort=followers&order=desc',
+  LowFoll: '&sort=followers&order=asc',
+};
