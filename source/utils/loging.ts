1f019c7524f92c6206bf938aba2007170ddfac65
Junior Programmer <progphyton@gmail.com>
2023-07-30 16:33:57 +0300

    feat: add loging function

---
+++ source/utils/loging.ts
@@ -0,0 +1,4 @@
+/* eslint-disable no-console */
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const loging = (...arg: any): void => console.log('Stamp: ', Date.now(), 'Data: ', ...arg);
