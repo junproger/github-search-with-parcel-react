e152112c9fb4dbcfbc98d0d3def822cb98c8cff7
Junior Programmer <progphyton@gmail.com>
2023-07-30 19:57:52 +0300

    feat: add fetch users hook to app

--- source/components/app/App.tsx
+++ source/components/app/App.tsx
@@ -1,22 +1,35 @@
 import React, { useState } from 'react';
 
 import github from '../../assets/github-logo.png';
+import Texts from '../../constants/textCollection';
+import QueriesType from '../../types/QueriesType';
 
 import { Header } from '../view/Header';
 import { Search } from '../control/Search';
-import Texts from '../../constants/textCollection';
+import { useFetchUsers } from '../model/useFetchUsers';
+import { loging } from 'utils/loging';
 
 export const App: React.FC = () => {
-  const [getQuery, setQuery] = useState('');
-  const callback = (value: string): void => {
-    setQuery(value);
-    // eslint-disable-next-line no-console
-    console.log(value, getQuery);
+  const [getQuery, setQuery] = useState<QueriesType>({
+    query: '',
+    sort: 'bestmatch',
+    order: 'desc',
+    page: 1,
+  });
+  const callquery = (value: string): void => {
+    setQuery((prev) => {
+      return {
+        ...prev,
+        query: value,
+      };
+    });
   };
+  const respData = useFetchUsers(getQuery);
+  loging(respData);
   return (
     <>
       <Header image={github} attrib="github" title={Texts.titleHeader} />
-      <Search callback={callback} holder={Texts.searchHolder} inform={Texts.informSearch} title={Texts.titleSearch} />
+      <Search callback={callquery} holder={Texts.searchHolder} inform={Texts.informSearch} title={Texts.titleSearch} />
     </>
   );
 };
