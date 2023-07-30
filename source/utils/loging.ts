1ab4cb5a64acd2b496f1334f6667a39799a75711
Junior Programmer <progphyton@gmail.com>
2023-07-30 17:52:40 +0300

    refactor: loging function

--- source/utils/loging.ts
+++ source/utils/loging.ts
@@ -1,4 +1,6 @@
 /* eslint-disable no-console */
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const loging = (...arg: any): void => console.log('Stamp: ', Date.now(), 'Data: ', ...arg);
+export const loging = (...arg: any): void => {
+  console.log('Stamp: ', Date.now(), 'Data: ', ...arg);
+};
