42c3838352bccfdf2b24dbe40503fe36aaadbf7f
Junior Programmer <progphyton@gmail.com>
2023-07-30 16:14:16 +0300

    fix: replace example query

--- source/constants/textCollection.ts
+++ source/constants/textCollection.ts
@@ -1,7 +1,7 @@
 enum textCollection {
   titleHeader = 'Github-Search React-App',
   informSearch = 'For enter print a name or part of name and press an enter key. For delete printed press a delete key.',
-  searchHolder = 'Enter a name or part of name, "octocat" for example.',
+  searchHolder = 'Enter a name or part of name, "junior" for example.',
   titleSearch = 'Enter a name or part of name and press an enter key.',
 }
 
