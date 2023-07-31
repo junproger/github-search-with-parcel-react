3de0dc2f7f0216d5bf5fc498df166c56f333cba3
Junior Programmer <progphyton@gmail.com>
2023-07-31 16:33:41 +0300

    fix: delete preset files

--- source/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { container } from './constants/container';
-import { createRoot } from 'react-dom/client';
-import { App } from './components/app/app';
-
-const ROOT = createRoot(container as HTMLElement);
-
-ROOT.render(<App />);
