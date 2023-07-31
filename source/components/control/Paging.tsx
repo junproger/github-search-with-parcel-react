c8b485141cd665aaa8b2fb58906a934a620af9d2
Junior Programmer <progphyton@gmail.com>
2023-08-01 00:46:17 +0300

    feat: add conditional rendering and clickhandle

--- source/components/control/Paging.tsx
+++ source/components/control/Paging.tsx
@@ -4,24 +4,37 @@ import * as styles from './paging.module.css';
 import { paging } from '../../utils/paging';
 
 export type Paging = {
+  handle: (page: number) => void;
   total: number | undefined;
   page: number;
 };
 
-export const Paging: React.FC<Paging> = ({ total, page }) => {
+export const Paging: React.FC<Paging> = ({ handle, total, page }) => {
   return (
     <nav className={styles['paging']}>
       <span>
         Total: <output>{total ? total : 0}</output>
       </span>
-      <strong>⬅ prev</strong>
+      {page >= 2 ? (
+        <strong className={styles['arrows']} onClick={(): void => handle(page - 1)}>
+          ⬅ prev
+        </strong>
+      ) : (
+        <span className={styles['disable']}>⇦ prev</span>
+      )}
       <span>
         Page:{' '}
         <output>
           {total ? page : 0} of {paging(total)}
         </output>
       </span>
-      <strong>next ➡</strong>
+      {page < paging(total) ? (
+        <strong className={styles['arrows']} onClick={(): void => handle(page + 1)}>
+          next ➡
+        </strong>
+      ) : (
+        <span className={styles['disable']}>next ⇨</span>
+      )}
     </nav>
   );
 };
