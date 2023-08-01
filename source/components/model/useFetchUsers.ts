0c92c32247f96a2f78dfcc90b06e22644d3fe559
Junior Programmer <progphyton@gmail.com>
2023-08-01 11:05:10 +0300

    feat: add checking and wrapping of response data

--- source/components/model/useFetchUsers.ts
+++ source/components/model/useFetchUsers.ts
@@ -1,27 +1,57 @@
 import { useEffect, useState } from 'react';
 
-import { loging } from '../../utils/loging';
-import { ISearchData } from '../../types/ISearchData';
 import QueriesType from '../../types/QueriesType';
 
-const useFetchUsers = (queries: QueriesType): ISearchData | null => {
-  const [getRespn, setRespn] = useState<ISearchData | null>(null);
-  const { query, squery, page } = queries;
+import { loging } from '../../utils/loging';
+import { ISearchData } from '../../types/ISearchData';
+import { ISearchError } from '../../types/ISearchError';
 
+export interface IFetchUsers {
+  done: boolean;
+  resp: ISearchData | null;
+  error: ISearchError | null;
+}
+
+const useFetchUsers = (queries: QueriesType): IFetchUsers => {
+  const [getRespn, setRespn] = useState<IFetchUsers>({
+    done: false,
+    resp: null,
+    error: null,
+  });
+  const { query, squery, page } = queries;
   useEffect(() => {
     if (!queries.query) {
-      setRespn(null);
+      setRespn({
+        done: true,
+        resp: null,
+        error: null,
+      });
       return;
     }
-    const fetchingUsers = async (): Promise<ISearchData> => {
+    const fetchingUsers = async (): Promise<ISearchData | ISearchError> => {
       const dataUsers = await fetch(`https://api.github.com/search/users?q=${query}${squery}&page=${page}`)
         .then((response) => response.json())
-        .then((data: ISearchData) => data)
+        .then((data: ISearchData | ISearchError) => data)
         .catch((err) => {
+          // loging errors
           loging(err);
+          alert(err);
           throw err;
         });
-      setRespn(dataUsers);
+      const isData = (data: ISearchData | ISearchError): data is ISearchData => true;
+      if (isData(dataUsers)) {
+        setRespn({
+          done: true,
+          resp: dataUsers,
+          error: null,
+        });
+      } else {
+        setRespn({
+          done: true,
+          resp: null,
+          error: dataUsers,
+        });
+      }
       return dataUsers;
     };
     fetchingUsers();
