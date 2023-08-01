630fa0732d63ae7a463020a8a77d800477fdc17e
Junior Programmer <progphyton@gmail.com>
2023-08-01 10:59:24 +0300

    feat: add error message to text collection

--- source/constants/textCollection.ts
+++ source/constants/textCollection.ts
@@ -3,6 +3,7 @@ enum textCollection {
   informSearch = 'For enter print a name or part of name and press an enter key. For delete printed press a delete key.',
   searchHolder = 'Enter a name or part of name, "junior" for example.',
   titleSearch = 'Enter a name or part of name and press an enter key.',
+  errorMessage = 'Enter a valid query including Aa-Zz and/or 0-9.',
 }
 
 export default textCollection;
