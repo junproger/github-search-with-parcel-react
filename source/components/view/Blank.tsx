fe4a320ba1cdf0eae5c7abb181bf2226f51d99ce
Junior Programmer <progphyton@gmail.com>
2023-08-01 11:27:04 +0300

    feat: add props with message to blank component

--- source/components/view/Blank.tsx
+++ source/components/view/Blank.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 import * as styles from './blank.module.css';
 
-export const Blank: React.FC = () => {
-  return <div className={styles['blank']}></div>;
+export type BlankType = {
+  message: string;
+};
+
+export const Blank: React.FC<BlankType> = ({ message }) => {
+  return (
+    <div className={styles['blank']}>
+      <h2>{message}</h2>
+    </div>
+  );
 };
