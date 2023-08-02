9ea1d793f1de855a781aeb231011a833cb9feaeb
Junior Programmer <progphyton@gmail.com>
2023-08-02 19:40:34 +0300

    fix: the example in placeholder

--- source/constants/textCollection.ts
+++ source/constants/textCollection.ts
@@ -1,7 +1,7 @@
 enum textCollection {
   titleHeader = 'Github-Search React-App',
   informSearch = 'For enter print a name or part of name and press an enter key. For delete printed press a delete key.',
-  searchHolder = 'Enter a name or part of name, "junior" for example.',
+  searchHolder = 'Enter a name or part of name, "balamut" for example.',
   titleSearch = 'Enter a name or part of name and press an enter key.',
   errorMessage = 'Enter a valid query including Aa-Zz and/or 0-9.',
 }
